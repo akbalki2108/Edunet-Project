62a8c94f17734442743fad5f3499c451a1365226
akbalki2108 <akbalki208012@gmail.com>
2024-03-13 00:01:03 +0530

    updated settings

--- edunet/settings.py
+++ edunet/settings.py
@@ -30,11 +30,12 @@ SECRET_KEY = "django-insecure-_wz^g$ope($-8-hru@geg1&^_=61z9hgzn#(oox%9h6a&5@sf7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'restaurantlyy.azurewebsites.net',
-    '*',
-]
+# ALLOWED_HOSTS = [
+#     'restaurantlyy.azurewebsites.net',
+#     '*',
+# ]
 
+ALLOWED_HOSTS = [os.environ['restaurantlyy.azurewebsites.net']] if 'restaurantlyy.azurewebsites.net' in os.environ else []
 
 # Application definition
 
